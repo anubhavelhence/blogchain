02f98050e7936ed06cdb7d1e624cbf5b9851698e
Anubhav <elhenceanubhav@gmail.com>
2022-08-31 08:06:24 +0000

    MAde query posts

--- x/blog/keeper/msg_server_create_post.go
+++ x/blog/keeper/msg_server_create_post.go
@@ -15,11 +15,11 @@ func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (
 		Creator: msg.Creator,
 		Title:   msg.Title,
 		Body:    msg.Body,
-	 }
-   
-	 // Add a post to the store and get back the ID
-	 id := k.AppendPost(ctx, post)
-   
-	 // Return the ID of the post
-	 return &types.MsgCreatePostResponse{Id: id}, nil
+	}
+
+	// Add a post to the store and get back the ID
+	id := k.AppendPost(ctx, post)
+
+	// Return the ID of the post
+	return &types.MsgCreatePostResponse{Id: id}, nil
 }
